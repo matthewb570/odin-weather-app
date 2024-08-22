572011b1e13a126aaa3f8606dd581e60e57b9dd2
Matthew Burgess <boog397@gmail.com>
2024-08-21 22:04:52 -0500

    Add HTML content for initial location search page

---
+++ src/js/locationInputPage.js
@@ -0,0 +1,55 @@
+class LocationInputPage {
+
+    createPage() {
+        const divLocationInputPage = document.createElement("div");
+        divLocationInputPage.id = "div-location-input-page";
+        divLocationInputPage.classList.add("page");
+        divLocationInputPage.appendChild(this.createPageHeader());
+        divLocationInputPage.appendChild(this.createPageContent());
+
+        return divLocationInputPage;
+    }
+
+    createPageHeader() {
+        const divPageHeader = document.createElement("div");
+        divPageHeader.classList.add("page-header");
+        divPageHeader.appendChild(this.createPageTitle());
+        
+        return divPageHeader;
+    }
+
+    createPageTitle() {
+        const divPageTitle = document.createElement("div");
+        divPageTitle.classList.add("page-title");
+        divPageTitle.textContent = "Weather App";
+
+        return divPageTitle;
+    }
+
+    createPageContent() {
+        const divPageContent = document.createElement("div");
+        divPageContent.classList.add("page-content");
+        divPageContent.appendChild(this.createLocationInputComponent());
+
+        return divPageContent;
+    }
+
+    createLocationInputComponent() {
+        const txtLocation = document.createElement("input");
+        txtLocation.id = "txt-location";
+        txtLocation.placeholder = "Show me the weather in...";
+        
+        const btnSearch = document.createElement("button");
+        btnSearch.type = "button";
+        btnSearch.textContent = "Search";
+
+        const divLocationInputComponent = document.createElement("div");
+        divLocationInputComponent.classList.add("location-search");
+        divLocationInputComponent.appendChild(txtLocation);
+        divLocationInputComponent.appendChild(btnSearch);
+
+        return divLocationInputComponent;
+    }
+}
+
+export default LocationInputPage;
