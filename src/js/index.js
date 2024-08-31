14e81f307d1816913d049cf0c91cc271b78727dd
Matthew Burgess <boog397@gmail.com>
2024-08-30 22:29:52 -0500

    Add initial rendering logic to index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,27 @@
 import "../css/styles.css";
+import VisualCrossingGateway from "./visualCrossingGateway";
+import WeatherDataModelMapper from "./weatherDataModelMapper";
+import LocationInputPage from "./locationInputPage";
+import WeatherPage from "./weatherPage";
 
-console.log("Hello World!");
+function searchFunction(searchValue) {
+    
+    let visualCrossingGateway = new VisualCrossingGateway();
+    
+    visualCrossingGateway.timeline(searchValue).then(json => {
+        
+        let weatherDataModelMapper = new WeatherDataModelMapper();
+
+        let weatherDataModel = weatherDataModelMapper.map(json);
+        
+        while(document.body.lastChild) {
+            document.body.removeChild(document.body.lastChild);
+        }
+
+        const weatherPage = new WeatherPage(weatherDataModel, searchFunction);
+        document.body.appendChild(weatherPage.createPage());
+    });
+}
+
+let locationInputPage = new LocationInputPage(searchFunction);
+document.body.appendChild(locationInputPage.createPage());
